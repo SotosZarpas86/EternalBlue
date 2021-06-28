80c23d2c3439994a41bf00ca13dfcfafbfd25431
Sotirios Zarpas <sotiriszarpas@gmail.com>
2021-06-28 10:42:32 +0300

    Add project files.

---
+++ EternalBlue.Web/Models/SignInModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace EternalBlue.Web.Models
+{
+    public class SignInModel
+    {
+        [Required]
+        public string Passowrd { get; set; }
+    }
+}
---
+++ EternalBlue.Web/Pages/SignIn.razor.cs
@@ -0,0 +1,57 @@
+﻿using Blazored.SessionStorage;
+using EternalBlue.Web.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace EternalBlue.Web.Pages
+{
+    public partial class SignIn
+    {
+        private SignInModel SignInModel { get; set; } = new();
+
+        [Inject]
+        protected ISyncSessionStorageService SessionStorageService { get; set; }
+
+        protected bool IsMainPageDisplayed { get; set; } = true;
+
+        protected bool IsTwoStepAuthenticationDisplayed { get; set; }
+
+        protected bool IsDownloadDisplayed { get; set; }
+
+        protected override void OnInitialized()
+        {
+            var isTwoStepAuthenticationPage = SessionStorageService.GetItem<bool>("TwoStepAuthenticationPage");
+            if (isTwoStepAuthenticationPage)
+            {
+                IsMainPageDisplayed = false;
+                IsTwoStepAuthenticationDisplayed = true;
+            }
+            var isDownloadPage = SessionStorageService.GetItem<bool>("DownloadPage");
+            if (isDownloadPage)
+            {
+                IsTwoStepAuthenticationDisplayed = false;
+                IsDownloadDisplayed = true;
+            }
+            base.OnInitialized();
+        }
+
+        private void HandleMainPageValidSubmit()
+        {
+            if (SignInModel.Passowrd.Equals("blueb1ueBlue"))
+            {
+                SessionStorageService.SetItem("TwoStepAuthenticationPage", true);
+                IsMainPageDisplayed = false;
+                IsTwoStepAuthenticationDisplayed = true;
+            }
+        }
+
+        private void HandleTwoStepAuthenticationValidSubmit()
+        {
+            if (SignInModel.Passowrd.Equals("blue1sF0rev3r"))
+            {
+                SessionStorageService.SetItem("DownloadPage", true);
+                IsTwoStepAuthenticationDisplayed = false;
+                IsDownloadDisplayed = true;
+            }
+        }
+    }
+}
